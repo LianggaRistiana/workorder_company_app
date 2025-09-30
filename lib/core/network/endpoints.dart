091b4aa80393320c0caae57d94f69d5e89da8f73
LianggaRistiana <liangga4@gmail.com>
2025-09-30 13:44:20 +0800

    fear(core) : add endpoints

--- lib/core/network/endpoints.dart
+++ lib/core/network/endpoints.dart
@@ -2,4 +2,14 @@ class Endpoints {
   // Auth
   static const String login = "/auth/login";
   static const String register = "/auth/register";
+  static const String logout = "/auth/register";
+  static const String registerCompany = "/auth/register-company";
+
+  // Company
+  static const String company = "/company"; // GET
+  static const String companies = '/companies'; // PUT, GET
+
+  static const String employees = "/companies/employees"; // GET, PUT
+  static const String inviteEmployees = "/companies/invite-employees"; // POST, DEL
+  static const String historyInvitations = "/companies/invite-history";// GET
 }
