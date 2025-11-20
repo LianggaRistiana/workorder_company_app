03bffd24610abda6b676beb68ecd6f927c6b473e
LianggaRistiana <liangga4@gmail.com>
2025-11-20 20:14:34 +0800

    style : update UI for filled form view

--- lib/features/client_service_request/presentation/pages/public_csr_detail_page.dart
+++ lib/features/client_service_request/presentation/pages/public_csr_detail_page.dart
@@ -3,13 +3,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:workorder_company_app/features/client_service_request/domain/entitties/client_service_request_entity.dart';
 import 'package:workorder_company_app/features/client_service_request/presentation/bloc/public_client_service_request/csr_bloc.dart';
 import 'package:workorder_company_app/features/client_service_request/presentation/bloc/public_client_service_request/csr_detail_cubit.dart';
-import 'package:workorder_company_app/core/constants/app_enums.dart';
-import 'package:workorder_company_app/features/forms/data/model/option_model.dart';
-import 'package:workorder_company_app/features/forms/domain/entities/field_entity.dart';
-import 'package:workorder_company_app/features/forms/domain/entities/option_entity.dart';
-import 'package:workorder_company_app/features/services/domain/entities/service_entity.dart';
-import 'package:workorder_company_app/features/submissions/data/model/field_data_model.dart';
-import 'package:workorder_company_app/features/submissions/domain/entitties/field_data_entity.dart';
+import 'package:workorder_company_app/shared/widgets/custom_list.dart';
+import 'package:workorder_company_app/shared/widgets/filled_form_view.dart';
 
 class CsrDetailPage extends StatefulWidget {
   final String csrId;
@@ -63,10 +58,12 @@ class _CsrDetailPageState extends State<CsrDetailPage> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 _header(csr),
-                const Divider(height: 32),
-                _infoSection(csr),
-                const Divider(height: 32),
-                _formsSection(csr),
+                if (csr.clientIntakeForms != null &&
+                    csr.clientIntakeForms!.isNotEmpty)
+                  CustomList(
+                      items: csr.clientIntakeForms!,
+                      itemBuilder: (item, _) =>
+                          FilledFormView(filledForm: item))
               ],
             ),
           );
@@ -89,167 +86,5 @@ class _CsrDetailPageState extends State<CsrDetailPage> {
         Chip(label: Text(csr.status.name.toUpperCase())),
       ],
     );
-  }
-
-  Widget _infoSection(csr) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text("Info CSR", style: Theme.of(context).textTheme.titleMedium),
-        const SizedBox(height: 12),
-        _infoTile("CSR ID", csr.id),
-        _infoTile("Tanggal Dibuat", csr.createdAt.toString()),
-        _infoTile("Company ID", csr.companyId),
-      ],
-    );
-  }
-
-  Widget _infoTile(String label, String value) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 8),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(width: 120, child: Text(label)),
-          Expanded(child: Text(value)),
-        ],
-      ),
-    );
-  }
-
-  Widget _formsSection(csr) {
-    final forms = csr.clientIntakeForms ?? [];
-
-    if (forms.isEmpty) {
-      return const Text("Tidak ada intake form.");
-    }
-
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          "Client Intake Forms",
-          style: Theme.of(context).textTheme.titleMedium,
-        ),
-        const SizedBox(height: 16),
-        ...forms.map((filled) {
-          final submission = filled.submission;
-
-          return Card(
-            elevation: 0.4,
-            child: ExpansionTile(
-              title: Text(filled.form.title),
-              subtitle: Text(
-                submission != null ? "Sudah diisi" : "Belum diisi",
-              ),
-              children: [
-                if (submission != null)
-                  Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: _fieldsView(filled),
-                  ),
-              ],
-            ),
-          );
-        }),
-      ],
-    );
-  }
-
-  Widget _fieldsView(filled) {
-    final formFields = filled.form.fields ?? [];
-    final submission = filled.submission;
-    final answers = submission?.fieldsData ?? [];
-
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [...formFields.map((field) {
-        final answer = answers
-            .firstWhere(
-              (f) => f.order == (field.order?.toString() ?? ""),
-              // FIXME : USE ENTITY INSTEAD OF MODEL, RN its error if using entity idk why
-              orElse: () => FieldDataModel(order: "", value: null),
-            )
-            .value;
-
-        return Padding(
-          padding: const EdgeInsets.only(bottom: 16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                field.label,
-                style: const TextStyle(fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 6),
-              _answerWidget(field, answer),
-            ],
-          ),
-        );
-      }),]
-    );
-  }
-
-  Widget _answerWidget(FieldEntity field, dynamic answer) {
-    if (answer == null || answer.toString().isEmpty) {
-      return const Text("-");
-    }
-
-    switch (field.type) {
-      case FieldType.text:
-      case FieldType.textarea:
-        return Text(answer.toString());
-
-      case FieldType.email:
-        return GestureDetector(
-          onTap: () {},
-          child: Text(
-            answer.toString(),
-            style: const TextStyle(
-              color: Colors.blue,
-              decoration: TextDecoration.underline,
-            ),
-          ),
-        );
-
-      case FieldType.number:
-        return Text(answer.toString());
-
-      case FieldType.date:
-        try {
-          final parsed = DateTime.tryParse(answer.toString());
-          return Text(parsed != null
-              ? "${parsed.day}-${parsed.month}-${parsed.year}"
-              : answer.toString());
-        } catch (_) {
-          return Text(answer.toString());
-        }
-
-      case FieldType.time:
-        return Text(answer.toString()); // jam biasanya sudah string "HH:mm"
-
-      case FieldType.singleSelect:
-        final opt = field.options?.firstWhere(
-          (o) => o.key == answer,
-          // FIXME : Use Entity
-          orElse: () => OptionModel(key: "", value: answer.toString()),
-        );
-        return Text(opt?.value ?? answer.toString());
-
-      case FieldType.multiSelect:
-        if (answer is List) {
-          final labels = answer.map((a) {
-            final opt = field.options?.firstWhere(
-              (o) => o.key == a,
-              // FIXME USE ENTITY
-              orElse: () => OptionModel(key: "", value: a.toString()),
-            );
-            return opt?.value ?? a.toString();
-          }).join(", ");
-
-          return Text(labels);
-        }
-        return Text(answer.toString());
-    }
   }
 }
---
+++ lib/shared/widgets/filled_form_view.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+import 'package:workorder_company_app/features/forms/domain/entities/field_entity.dart';
+import 'package:workorder_company_app/features/forms/domain/entities/filled_form_entity.dart';
+import 'package:workorder_company_app/core/constants/app_enums.dart';
+import 'package:workorder_company_app/features/forms/domain/entities/option_entity.dart';
+import 'package:workorder_company_app/shared/widgets/custom_card.dart';
+
+class FilledFormView extends StatelessWidget {
+  final FilledFormEntity filledForm;
+
+  const FilledFormView({super.key, required this.filledForm});
+
+  // -------------------------------------------------------------
+  // FIELD WRAPPER
+  // -------------------------------------------------------------
+  Widget _filledField(BuildContext context, FieldEntity field, dynamic answer) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          field.label,
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
+        _answerWidget(context, field, answer),
+        const SizedBox(height: 16),
+      ],
+    );
+  }
+
+  // -------------------------------------------------------------
+  // ANSWER WIDGET
+  // -------------------------------------------------------------
+  Widget _answerWidget(
+      BuildContext context, FieldEntity field, dynamic answer) {
+    switch (field.type) {
+      case FieldType.text:
+      case FieldType.textarea:
+      case FieldType.time:
+      case FieldType.number:
+        if (answer == null || answer.toString().isEmpty) {
+          return _textAnswer('-');
+        }
+        return _textAnswer(answer.toString());
+
+      case FieldType.email:
+        return GestureDetector(
+          onTap: () {},
+          child: Text(
+            answer.toString(),
+            style: const TextStyle(
+              color: Colors.blue,
+              decoration: TextDecoration.underline,
+            ),
+          ),
+        );
+
+      case FieldType.date:
+        try {
+          final parsed = DateTime.tryParse(answer.toString());
+          return Text(parsed != null
+              ? "${parsed.day}-${parsed.month}-${parsed.year}"
+              : answer.toString());
+        } catch (_) {
+          return Text(answer.toString());
+        }
+
+      case FieldType.singleSelect:
+        return _optionAnswer(context, field.options ?? [], [answer.toString()]);
+
+      case FieldType.multiSelect:
+        final listAnswer =
+            (answer as List?)?.map((e) => e.toString()).toList() ?? [];
+        return _optionAnswer(context, field.options ?? [], listAnswer);
+    }
+  }
+
+  // -------------------------------------------------------------
+  // TEXT ANSWER
+  // -------------------------------------------------------------
+  Widget _textAnswer(String answer) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(answer),
+        const Divider(height: 2),
+      ],
+    );
+  }
+
+  // -------------------------------------------------------------
+  // OPTION ANSWER (CHECKBOXES)
+  // -------------------------------------------------------------
+  Widget _optionAnswer(BuildContext context, List<OptionEntity> options,
+      List<String> keyAnswer) {
+    if (options.isEmpty) {
+      return const Text("-");
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: options.map((opt) {
+        final isSelected = keyAnswer.contains(opt.key);
+
+        return Row(
+          children: [
+            Icon(
+              isSelected ? Icons.check_box : Icons.check_box_outline_blank,
+              size: 20,
+              color: isSelected ? Theme.of(context).primaryColor : null,
+            ),
+            const SizedBox(width: 8),
+            Expanded(child: Text(opt.value)),
+          ],
+        );
+      }).toList(),
+    );
+  }
+
+  // -------------------------------------------------------------
+  // FIND ANSWER BASED ON ORDER
+  // -------------------------------------------------------------
+  dynamic _findAnswer(String order) {
+    final submission = filledForm.submission;
+    if (submission == null || submission.fieldsData == null) {
+      return null;
+    }
+
+    final matched = submission.fieldsData!.where((f) => f.order == order);
+
+    return matched.isNotEmpty ? matched.first.value : null;
+  }
+
+  // -------------------------------------------------------------
+  // BUILD WIDGET
+  // -------------------------------------------------------------
+  @override
+  Widget build(BuildContext context) {
+    return CustomCard(
+      borderColor: Colors.black.withAlpha(50),
+      padding: const EdgeInsets.all(0),
+      margin: const EdgeInsets.only(bottom: 16, top: 4),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(8),
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(12),
+                topRight: Radius.circular(12),
+              ),
+            ),
+            child: Text('${filledForm.order}. ${filledForm.form.title}',
+                style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                      color: Colors.white,
+                    )),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ...filledForm.form.fields!.map(
+                  (field) => _filledField(
+                    context,
+                    field,
+                    _findAnswer(field.order.toString()),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
