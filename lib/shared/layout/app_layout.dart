72a8e4d7c2b9f1692b35d81d3fc998a45e39fc25
LianggaRistiana <liangga4@gmail.com>
2026-04-07 20:06:44 +0800

    refactor : splash page into shared feature and rename it into loading page

--- lib/shared/layout/app_layout.dart
+++ lib/shared/layout/app_layout.dart
@@ -3,11 +3,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:workorder_company_app/core/constants/app_enums.dart';
 import 'package:workorder_company_app/features/auth/presentation/bloc/auth_bloc.dart';
-import 'package:workorder_company_app/features/splash/splash_page.dart';
+import 'package:workorder_company_app/shared/page/loading_page.dart';
 import 'package:workorder_company_app/routes/app_routes.dart';
 import 'package:workorder_company_app/shared/layout/navigation/app_navigantion_bar.dart';
 
-// ===================== WIHT OUT ANIMATE ==============================
 class AppLayout extends StatelessWidget {
   final Widget child;
 
@@ -19,7 +18,7 @@ class AppLayout extends StatelessWidget {
     final path = GoRouterState.of(context).uri.path;
 
     if (authState is! Authenticated) {
-      return SplashPage();
+      return LoadingPage();
     }
 
     final role = authState.user.role;
@@ -34,7 +33,11 @@ class AppLayout extends StatelessWidget {
 
 final Map<UserRole, List<String>> _roleMainPages = {
   UserRole.ownerCompany: [AppRoutes.home, AppRoutes.companyManageMenu],
-  UserRole.managerCompany: [AppRoutes.home, AppRoutes.serviceRequestInbox, AppRoutes.workorders],
+  UserRole.managerCompany: [
+    AppRoutes.home,
+    AppRoutes.serviceRequestInbox,
+    AppRoutes.workorders
+  ],
   UserRole.staffCompany: [AppRoutes.home, AppRoutes.workorders],
   UserRole.staffUnassigned: [AppRoutes.home, AppRoutes.invitationsPending],
   UserRole.client: [AppRoutes.home, AppRoutes.publicCompanies],
--- lib/shared/page/loading_page.dart
+++ lib/shared/page/loading_page.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:workorder_company_app/shared/widgets/app_loading.dart';
 
-class SplashPage extends StatefulWidget {
-  const SplashPage({super.key});
+class LoadingPage extends StatefulWidget {
+  const LoadingPage({super.key});
 
   @override
-  State<SplashPage> createState() => _SplashPageState();
+  State<LoadingPage> createState() => _SplashPageState();
 }
 
-class _SplashPageState extends State<SplashPage> {
+class _SplashPageState extends State<LoadingPage> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
