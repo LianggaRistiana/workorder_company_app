7130b8d879db5692db7359c89b0d0a0a4e282dd4
LianggaRistiana <liangga4@gmail.com>
2026-04-22 21:22:50 +0800

    refactor : remove services legacy code

--- lib/features_legacy/services_legacy/domain/repositories/service_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:workorder_company_app/core/types/future_either.dart';
-import 'package:workorder_company_app/features_legacy/services_legacy/domain/entities/service_entity.dart';
-
-abstract class ServiceRepository {
-  FutureEitherList<ServiceEntity> getServices();
-  FutureEither<ServiceEntity> getService(String id);
-  FutureEither<void> createService(ServiceEntity service);
-}
